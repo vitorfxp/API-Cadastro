7c2350530fe83024eca8470eb59e172222a2d986
João Vitor Ferreira Ribeiro <joaovitorpvh777@gmail.com>
2026-05-30 10:46:01 -0400

    first commit

---
+++ src/test/java/dev/jv/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jv.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
