2c3ec1047df47548c09a7bef5dd61ef736733285
João Vitor Ferreira Ribeiro <joaovitorpvh777@gmail.com>
2026-05-31 10:08:12 -0400

    configurando classe + tabela ninja

--- src/main/java/dev/jv/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/jv/CadastroDeNinjas/NinjaController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// RestController é uma anotation para indicar que essa classe é um controller
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
 
+
+    // ROTA QUE VOCÊ NECESSITA (GET, POST, PUT, PATCH,DELETE + Mappping
     @GetMapping("/primeira")
     public String boasVindas(){
         return "Essa é a minha primeira rota com uma api rest";
