e93acc96c139f3af6ba79049b8a823d440255efb
João Vitor Ferreira Ribeiro <02788678207@sedam.ro.gov.br>
2026-06-01 09:00:40 -0400

    FEAT:Adição dos repositorys

---
+++ src/main/java/dev/jv/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,9 @@
+package dev.jv.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface MissoesRepository extends JpaRepository<MissaoModel, Long> {
+
+}
