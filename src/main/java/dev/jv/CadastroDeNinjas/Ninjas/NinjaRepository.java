bdecaa72d40508774593fee6787657b7eb6d30ce
João Vitor Ferreira Ribeiro <02788678207@sedam.ro.gov.br>
2026-06-03 13:25:55 -0400

    teste

--- src/main/java/dev/jv/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/jv/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,10 @@
 package dev.jv.CadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// JPA é uma abstração do springboot no java quando precisamos fazer querys/lidar com banco de dados.
+// JPA é basicamente um facilitador  jpa<(qual classe o jpa vai querer que orm fique escaneando),(tipo do id )>
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+
 }
